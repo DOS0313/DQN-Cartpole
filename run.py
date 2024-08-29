7519073ec9b0aef8770c4cb8966d3be3fd8937be
DO_S <rlaxogus7094@naver.com>
2024-08-29 23:17:59 +0900

    feat : DQN Cartpole 예제 구축

--- run.py
+++ run.py
@@ -1,17 +1,20 @@
-import gymnasium as gym
+import gym
 import math
 import random
+import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 from collections import namedtuple, deque
 from itertools import count
+from PIL import Image
 
 import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
+import torchvision.transforms as T
 
-env = gym.make("CartPole-v1")
+env = gym.make('CartPole-v1', render_mode="rgb_array").unwrapped
 
 # matplotlib 설정
 is_ipython = 'inline' in matplotlib.get_backend()
@@ -20,5 +23,240 @@ if is_ipython:
 
 plt.ion()
 
-# GPU를 사용할 경우
+# GPU를 사용할 수 있으면 사용하고, 아니면 CPU를 사용한다.
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+# ReplayMemory를 정의
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        # deque는 양방향 queue를 의미한다.
+        self.memory = deque([],maxlen=capacity)
+
+    def push(self, *args):
+        # Transition을 저장하는 부분이다.
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        # memory로부터 batch_size 길이 만큼의 list를 반환한다.
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        # memory의 길이를 반환한다.
+        return len(self.memory)
+
+# DQN 코드
+# Network에서 하고 싶은 것은 현재의 입력에 대한 각 행동의 기대값을 예측하는 것이다.
+# Cart-Pole 예제에서 선택할 수 있는 action은 왼쪽 혹은 오른쪽으로 cart를 이동하는 것이다.
+# 따라서, Q(s,left)와 Q(s,right)가 network의 출력이 된다.
+
+class DQN(nn.Module):
+    def __init__(self, h, w, outputs):
+        super(DQN, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=5, stride=2)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=5, stride=2)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=5, stride=2)
+        self.bn3 = nn.BatchNorm2d(32)
+
+        def conv2d_size_out(size, kernel_size = 5, stride = 2):
+            return (size - (kernel_size - 1) - 1) // stride  + 1
+        convw = conv2d_size_out(conv2d_size_out(conv2d_size_out(w)))
+        convh = conv2d_size_out(conv2d_size_out(conv2d_size_out(h)))
+        linear_input_size = convw * convh * 32
+        self.head = nn.Linear(linear_input_size, outputs)
+
+    def forward(self, x):
+        x = x.to(device)
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        rst = self.head(x.view(x.size(0), -1))
+        return rst
+
+resize = T.Compose([T.ToPILImage(),
+                    T.Resize(40, interpolation=T.InterpolationMode.BICUBIC),
+                    T.ToTensor()])
+
+def get_cart_location(screen_width):
+    world_width = env.x_threshold * 2
+    scale = screen_width / world_width
+    return int(env.state[0] * scale + screen_width / 2.0)  # MIDDLE OF CART
+
+def get_screen():
+    screen = env.render().transpose((2, 0, 1))
+
+    _, screen_height, screen_width = screen.shape
+    screen = screen[:, int(screen_height*0.4):int(screen_height * 0.8)]
+    view_width = int(screen_width * 0.6)
+    cart_location = get_cart_location(screen_width)
+    if cart_location < view_width // 2:
+        slice_range = slice(view_width)
+    elif cart_location > (screen_width - view_width // 2):
+        slice_range = slice(-view_width, None)
+    else:
+        slice_range = slice(cart_location - view_width // 2,
+                            cart_location + view_width // 2)
+
+    screen = screen[:, :, slice_range]
+    screen = np.ascontiguousarray(screen, dtype=np.float32) / 255
+    screen = torch.from_numpy(screen)
+
+    return resize(screen).unsqueeze(0)
+
+# 예제 patch를 출력해볼 수 있는 부분
+env.reset()
+plt.figure()
+plt.imshow(get_screen().cpu().squeeze(0).permute(1, 2, 0).numpy(),
+           interpolation='none')
+plt.title('Example extracted screen')
+plt.show()
+
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
+
+init_screen = get_screen()
+
+_, _, screen_height, screen_width = init_screen.shape
+
+n_actions = env.action_space.n
+
+policy_net = DQN(screen_height, screen_width, n_actions).to(device)
+target_net = DQN(screen_height, screen_width, n_actions).to(device)
+
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
+
+optimizer = optim.RMSprop(policy_net.parameters())
+
+# Capacity (즉, maximum length) 10000짜리 deque 이다.
+memory = ReplayMemory(10000)
+steps_done = 0
+
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+    math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
+
+    if sample > eps_threshold:
+        with torch.no_grad():
+            return policy_net(state).max(1)[1].view(1, 1)
+    else:
+        return torch.tensor([[random.randrange(n_actions)]], device=device, \
+        dtype=torch.long)
+
+episode_durations = []
+
+def plot_durations():
+    plt.figure(2)
+    plt.clf()
+    durations_t = torch.tensor(episode_durations, dtype=torch.float)
+    plt.title('Training...')
+    plt.xlabel('Episode')
+    plt.ylabel('Duration')
+    plt.plot(durations_t.numpy())
+    # 100개 에피소드의 평균을 가져 와서 도표 그리기
+    if len(durations_t) >= 100:
+        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
+        means = torch.cat((torch.zeros(99), means))
+        plt.plot(means.numpy())
+
+    plt.pause(0.001)  # 도표가 업데이트되도록 잠시 멈춤
+    if is_ipython:
+        display.clear_output(wait=True)
+        display.display(plt.gcf())
+
+
+def optimize_model():
+    if len(memory) < BATCH_SIZE:
+        return
+
+    transitions = memory.sample(BATCH_SIZE)
+
+    batch = Transition(*zip(*transitions))
+
+    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
+                                            batch.next_state)), device=device, dtype=torch.bool)
+
+    non_final_next_states = torch.cat([s for s in batch.next_state
+                                       if s is not None])
+
+    state_batch = torch.cat(batch.state)
+    action_batch = torch.cat(batch.action)
+    reward_batch = torch.cat(batch.reward)
+
+    state_action_values = policy_net(state_batch).gather(1, action_batch)
+
+    next_state_values = torch.zeros(BATCH_SIZE, device=device)
+
+    next_state_values[non_final_mask] = target_net(non_final_next_states).max(1)[0].detach()
+
+    # expected Q value를 계산
+    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
+
+    # Huber Loss 계산
+    criterion = nn.SmoothL1Loss()
+    loss = criterion(state_action_values, expected_state_action_values.unsqueeze(1))
+
+    # Optimize parameters
+    optimizer.zero_grad()
+    loss.backward()
+    for param in policy_net.parameters():
+        param.grad.data.clamp_(-1, 1)
+    optimizer.step()
+
+
+num_episodes = 100
+for i_episode in range(num_episodes):
+    # env와 state를 초기화 한다.
+    env.reset()
+    last_screen = get_screen()
+    current_screen = get_screen()
+    state = current_screen - last_screen
+
+    for t in count():
+        action = select_action(state)
+
+        _, reward, done, _, _ = env.step(action.item())
+        reward = torch.tensor([reward], device=device)
+
+        last_screen = current_screen
+        current_screen = get_screen()
+
+        if not done:
+            next_state = current_screen - last_screen
+        else:
+            next_state = None
+
+        memory.push(state, action, next_state, reward)
+
+        # 다음 상태로 이동
+        state = next_state
+
+        # (policy network에서) 최적화 한단계 수행
+        optimize_model()
+
+        if done:
+            episode_durations.append(t + 1)
+            plot_durations()
+            plt.show()
+            break
+
+    if i_episode % TARGET_UPDATE == 0:
+        target_net.load_state_dict(policy_net.state_dict())
+
+# 학습 마무리.
+print('Complete')
+env.render()
+env.close()
+plt.ioff()
+plt.show()
